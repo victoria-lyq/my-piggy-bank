c9333da708debe46a3f8a0f3683a5546a37c30e5
victoria-lyq <yaqiliu@bu.edu>
2024-09-04 20:47:21 -0700

    copy over files

---
+++ web/app/controllers/accounts_controller.ts
@@ -0,0 +1,6 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class AccountsController {
+  async index({}: HttpContext) {}
+  async refresh({}: HttpContext) {}
+}
---
+++ web/database/migrations/1724202354246_create_transactions_table.ts
@@ -0,0 +1,42 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'transactions'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.foreign('account_id')
+      table.double('amount')
+      table.string('iso_currency_code').nullable()
+      table.string('unofficial_currency_code').nullable()
+      table.string('iso_currency_code').nullable()
+      table.string('check_number').nullable()
+      table.date('posted_date').nullable()
+      table.date('authorized_date').nullable()
+      table.date('posted_datetime').nullable()
+      table.date('authorized_datetime').nullable()
+      table.geography('location').nullable()
+      table.string('merchant_name').nullable()
+      table.string('original_description').nullable()
+      // table.string('payment_meta').nullable()
+      table.boolean('pending')
+      table.string('pending_transaction_id').nullable()
+      table.string('account_owner').nullable()
+      table.index('transaction_id')
+      table.string('logo_url').nullable()
+      table.string('website').nullable()
+      table.enum('payment_channel', ["online", "in store", "other"])
+      table.string('personal_finance_category_primary')
+      table.string('personal_finance_category_detailed')
+      table.string('personal_finance_category_ci').nullable()
+      table.string('transaction_code').nullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
